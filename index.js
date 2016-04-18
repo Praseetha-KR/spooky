d825ab669ccff1753d641bcfe4f9f91b919cfb37
Praseetha-KR <voidimagineer@yahoo.com>
2016-04-19 04:42:42 +0530

    Integrated socket.io

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 'use strict';
 
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var PORT = 3300;
 
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
+io.on('connection', (socket) => {
+    console.log('user connected ');
+    socket.on('disconnect', () => console.log('user disconnected'));
+});
+
 http.listen(PORT, () => console.log('Listening on *: ' + PORT));
+
+
