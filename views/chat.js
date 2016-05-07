f496a1da028ddf80196a55dada96e68331fffe38
Praseetha-KR <voidimagineer@yahoo.com>
2016-05-08 04:09:21 +0530

    Add react message rendering with dummy data

--- views/chat.js
+++ views/chat.js
@@ -1,17 +1,64 @@
 'use strict';
 
-var chatApp = React.createClass({
-    // getInitialState: function() {
-    //     return { messages: [], text: '' };
-    // },
+var Message = React.createClass({
     render: function() {
         return (
-            console.log('return')
+            <div className="message">
+                <div className="messageText">{this.props.text}</div>
+                <div className="messageSender">{this.props.sender}</div>
+            </div>
         );
     }
 });
 
+var MessageList = React.createClass({
+    render: function() {
+        return (
+            <div className="messageList">
+            {
+                this.props.messages.map((msg, i) => {
+                    return(
+                        <Message
+                            key={i}
+                            text={msg.text}
+                            sender={msg.sender}
+                        />
+                    )
+                })
+            }
+            </div>
+        )
+    }
+})
+var ChatApp = React.createClass({
+    getData: function() {
+        return [
+            {
+                text: 'Lumos 🔦',
+                sender: 'Harry'
+            },
+            {
+                text: 'Wingardium leviosa 🍃',
+                sender: 'Hermione'
+            },
+            {
+                text: 'Avada kedavra 👿🐍💎',
+                sender: 'Voldemort'
+            }
+        ]
+    },
+    render: function() {
+        return (
+            <div className="chatApp">
+                Welcome to Spooky chat!
+                <MessageList messages={this.getData()} />
+            </div>
+        )
+    }
+});
+
 ReactDOM.render(
-    <chatApp />,
-    document.getElementById('test')
+    <ChatApp />,
+    document.getElementById('msglist')
 );
+
